6ab9f80172a41aa7c02ad708e9f7d25fc6778964
jopoepl <joel_joseph93@outlook.com>
2022-08-18 13:34:45 +0530

    update

--- block-BJaajy/code/index.js
+++ block-BJaajy/code/index.js
@@ -1,7 +1,17 @@
-Promise.all([
-    new Promise((resolve, reject) => {
-      setTimeout(() => resolve('Arya'), 1000);
-    }),
-    'Sam',
-    { name: 'John' },
-  ]).then(console.log);
+function loadImage(url) {
+    return new Promise((res, rej) => {
+     let img = new Image();
+     img.addEventListener("load" , () => res(img))
+    img.addEventListener("error" , () => rej(img))
+    img.src = url;
+    
+     })
+     
+      
+    }
+    loadImage('https://images.unsplash.com/photo-1614157510257-968fb5129989')
+      .then((img) => {
+        document.body.append(img);
+        console.log(`w: ${img.width} | h: ${img.height}`);
+      })
+      .catch((err) => console.error(err));
